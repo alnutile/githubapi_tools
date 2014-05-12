1643f5a15a5a23a6623ca880742ad902dae7b708
alnutile <alfrednutile@gmail.com>
2014-05-12 12:27:44 -0400

    Initialize

---
+++ github.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'vendor/autoload.php';
+use Github\Client;
+use Github\ResultPager;
+use Keboola\Csv\CsvFile;
+
+\Dotenv::load(__DIR__);
+
+$client = new Client();
+
+$user_name = $_ENV['USER_NAME'];
+$token = $_ENV['TOKEN'];
+$orgname = $_ENV['ORG_NAME'];
+
+$client->authenticate($user_name, $token, null);
+
+$members_array = [];
+
+$results = new ResultPager($client);
+
+$all_members = $results->fetchAll($client->api('organization')->members(), 'all', [$orgname]);
+foreach($all_members as $value) {
+    $user_info = 'too hard to get via api';
+    $email = $user_info;
+    $members_array[] = [
+      'username' => $value['login'],
+      'email'    => $email
+    ];
+}
+
+$csvFile = new CsvFile(__DIR__ . '/members.csv');
+
+foreach ($members_array as $row) {
+    $csvFile->writeRow($row);
+}
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Get info about your org
+
+### 1. Setup
+
+Make sure to set your environment variables
+
+copy the evn file to .env and put in the right info
